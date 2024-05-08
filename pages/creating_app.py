a22e2434618ac0f0b8a9195224cf714bba8f39a9
jamielee <85136381+lleejamiee@users.noreply.github.com>
2024-05-08 16:39:46 +1200

    feat[create]: Add reference material enabled

--- pages/creating_app.py
+++ pages/creating_app.py
@@ -7,21 +7,22 @@ client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 # Prompt for LLM
 prompt = """
 You are a technical writer tasked with creating a user guide for a product/service. 
-Your task is to create a user guide by incorporating relevant information from the provided outdated user guide.
+Your task is to create a user guide by incorporating relevant information from the provided outdated user guide, and the reference material.
 Only generate the content that is to be used in this new user guide.
 Do not provide explanations or notes.
 
 The user will input one text document
 Document 1: Outdated User Guide
+Document 2: Reference Material
 """
 
 # llm on local inference server(LM Studio)
-def generate_text(outdated_guide):
+def generate_text(outdated_guide, reference_material):
     history = [
         {"role": "system",
          "content": prompt},
         {"role": "user",
-         "content": f"Outdated User Guide:\n{outdated_guide}"}
+         "content": f"Outdated User Guide:\n{outdated_guide}\n\nReference Material:\n{reference_material}"}
     ]
 
     completion = client.chat.completions.create(
@@ -39,25 +40,28 @@ def generate_text(outdated_guide):
     return generated_text
 
 def main():
-    st.set_page_config(page_title="User Guide Creator")
+    st.set_page_config(page_title="User Guide Updater")
 
     # Set app header
-    st.header("Create a user guide")
+    st.header("Update a user guide")
 
     # Upload both outdated user guide and reference material
     outdated_guide_file = st.file_uploader("Choose the outdated user guide .txt file...", type="txt") # Can also use doc type
+    reference_material_file = st.file_uploader("Choose the reference material .txt file...", type="txt")
 
-    if outdated_guide_file is not None:
+    if outdated_guide_file is not None and reference_material_file is not None:
         # Read the contents of both files
         outdated_guide = outdated_guide_file.read().decode("utf-8")
+        reference_material = reference_material_file.read().decode("utf-8")
 
         # Uncomment to display the contents of each .txt file on the app
         #st.write(outdated_guide)
+        #st.write(reference_material)
 
         st.subheader("Updated User Guide Content")
 
         # Generate the updated user guide content using LLM
-        updated_guide = generate_text(outdated_guide)
+        updated_guide = generate_text(outdated_guide, reference_material)
 
         # Display the generated text on the app
         st.write(updated_guide)
@@ -73,7 +77,7 @@ def main():
         st.download_button("Download", updated_guide)
 
     else:
-        st.error("Please upload outdate file.")
+        st.error("Please upload both files.")
 
 if __name__ == '__main__':
     main()
