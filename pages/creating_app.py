48f056f15188b1e5d03932775489a36c10cc7418
Philip Jin <philipjin4@gmail.com>
2024-05-15 17:22:50 +1200

    feat[create]: Added functionality for uploading docx & pdf files

--- pages/creating_app.py
+++ pages/creating_app.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
+import docx2txt
 import streamlit as st
+import pdfplumber
 
 # Initialize OpenAI client with local inference server (LM Studio)
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
@@ -15,6 +17,7 @@ The user will input one text document
 Document 1: Outdated User Guide
 Document 2: Reference Material
 """
+
 
 # llm on local inference server(LM Studio)
 def generate_text(outdated_guide, reference_material):
@@ -39,6 +42,23 @@ def generate_text(outdated_guide, reference_material):
 
     return generated_text
 
+
+def extract_text(file):
+    if file.type == "text/plain":
+        file_text = str(file.read(), "utf-8")
+    elif file.type == "application/pdf":
+        try:
+            with pdfplumber.open(file) as pdf:
+                pages = pdf.pages[0]
+                file_text = pages.extract_text()
+        except:
+            return
+    else:
+        file_text = docx2txt.process(file)
+
+    return file_text
+
+
 def main():
     st.set_page_config(page_title="User Guide Updater")
 
@@ -46,38 +66,38 @@ def main():
     st.header("Update a user guide")
 
     # Upload both outdated user guide and reference material
-    outdated_guide_file = st.file_uploader("Choose the outdated user guide .txt file...", type="txt") # Can also use doc type
-    reference_material_file = st.file_uploader("Choose the reference material .txt file...", type="txt")
+    outdated_guide_file = st.file_uploader("Choose the outdated user guide document", type=["txt", "pdf", "docx"])
+    reference_material_file = st.file_uploader("Choose the reference material document", type=["txt", "pdf", "docx"])
 
     if outdated_guide_file is not None and reference_material_file is not None:
-        # Read the contents of both files
-        outdated_guide = outdated_guide_file.read().decode("utf-8")
-        reference_material = reference_material_file.read().decode("utf-8")
+        outdated_guide_text = extract_text(outdated_guide_file)
+        reference_material_text = extract_text(reference_material_file)
 
-        # Uncomment to display the contents of each .txt file on the app
-        #st.write(outdated_guide)
-        #st.write(reference_material)
+        # Uncomment to display the contents of each .docx file on the app
+        st.write(outdated_guide_text)
+        st.write(reference_material_text)
 
         st.subheader("Updated User Guide Content")
 
         # Generate the updated user guide content using LLM
-        updated_guide = generate_text(outdated_guide, reference_material)
+        updated_guide = generate_text(outdated_guide_text, reference_material_text)
 
         # Display the generated text on the app
         st.write(updated_guide)
 
         st.subheader("Download the Updated User Guide")
 
-        # Create .txt file
-        with open("new_guide.txt", "w") as f:
+        # Create .docx file
+        with open("updated_guide.docx", "w") as f:
             # Write from generated text
             f.write(updated_guide)
 
-        # Allow app user to download .txt file(updated user guide file) containing generated text
+        # Allow app user to download .docx file(updated user guide file) containing generated text
         st.download_button("Download", updated_guide)
 
     else:
         st.error("Please upload both files.")
+
 
 if __name__ == '__main__':
     main()
