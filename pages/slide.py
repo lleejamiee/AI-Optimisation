d9a70c898efebc522ad70a6bed6f3a8593d8c954
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2024-09-01 19:52:30 +1200

    feat[slides]: Add llama index SimpleWebPage Reader. Need to fix build_query_engine function

--- pages/slide.py
+++ pages/slide.py
@@ -1,27 +1,25 @@
-from io import BytesIO
 import streamlit as st
 from utilities.slide_processor import SlideProcessor
-from utilities.document_processor import DocumentProcessor
 
-query_json = """{
-    "input_text": "[[content]]",
-    "output_format": "json",
-    "json_structure": {
-    "slide.py": "{{presentation_slides}}"
-    }
-}"""
-
-# Prompt for LLM
-prompt = """
-You are a technical writer tasked with creating a user guide for a product/service. 
-Your task is to update a user guide powerpoint by incorporating relevant information from the provided outdated user guide, and the reference material.
-Only generate the content that is to be used in this new user guide.
-Do not provide explanations or notes.
-
-The user will input one text document
-Document 1: Outdated User Guide
-Document 2: Reference Material
-"""
+# query_json = """{
+#     "input_text": "[[content]]",
+#     "output_format": "json",
+#     "json_structure": {
+#     "slide.py": "{{presentation_slides}}"
+#     }
+# }"""
+#
+# # Prompt for LLM
+# prompt = """
+# You are a technical writer tasked with creating a user guide for a product/service.
+# Your task is to update a user guide powerpoint by incorporating relevant information from the provided outdated user guide, and the reference material.
+# Only generate the content that is to be used in this new user guide.
+# Do not provide explanations or notes.
+#
+# The user will input one text document
+# Document 1: Outdated User Guide
+# Document 2: Reference Material
+# """
 
 
 def main():
@@ -40,62 +38,66 @@ def main():
 
     if st.button("Generate Updated Guide"):
         if outdated_guide_file is not None:
+            outdated_path = SlideProcessor.save_uploaded_file(outdated_guide_file)
+            st.session_state.outdated_doc = SlideProcessor.load_document(outdated_path)
 
             if reference_material_file or webpage_link is not None:
                 if reference_material_file:
-                    st.session_state.outdated_guide_text, st.session_state.reference_material_text = (
-                        DocumentProcessor.read_files(outdated_guide_file, reference_material_file))
-                    st.session_state.generated_text = SlideProcessor.generate_presentation_content(st.session_state.outdated_guide_text, st.session_state.reference_material_text)
+                    reference_path = SlideProcessor.save_uploaded_file(reference_material_file)
+                    st.session_state.reference_doc = SlideProcessor.load_document(reference_path)
 
                 if webpage_link:
-                    st.session_state.outdated_guide_text = DocumentProcessor.extract_text(outdated_guide_file)
-                    st.session_state.reference_material_text = DocumentProcessor.extract_url_text(webpage_link)
-                    st.session_state.generated_text = SlideProcessor.generate_presentation_content(st.session_state.outdated_guide_text, st.session_state.reference_material_text)
+                    st.session_state.reference_doc = SlideProcessor.load_webpage(webpage_link)
             else:
                 st.error("Please upload either reference material or webpage link.")
 
         else:
             st.error("Please upload outdated material.")
 
-    if st.session_state.generated_text:
-        st.subheader("Updated User Guide Content")
+    if 'outdated_doc' and 'reference_doc' in st.session_state:
+        print("I'm here")
+        # s_engine = SlideProcessor.build_query_engine(st.session_state.outdated_doc, st.session_state.reference_doc)
 
-        if "edit_mode" not in st.session_state:
-            st.session_state.edit_mode = False
 
-        outdated_guide_compared, updated_guide_compared = DocumentProcessor.compare_texts(
-            st.session_state.outdated_guide_text, st.session_state.generated_text)
-
-        if st.session_state.edit_mode:
-            DocumentProcessor.edit_output(updated_guide_compared)
-        else:
-            DocumentProcessor.display_output(outdated_guide_compared, updated_guide_compared)
-
-        def regenerate_guide():
-            st.session_state.generated_text = SlideProcessor.generate_presentation_content(st.session_state.outdated_guide_text, st.session_state.reference_material_text)
-
-        st.button("Re-Generate", on_click=regenerate_guide)
-
-        # TextProcessor.select_version()
-
-        # outdated_guide_slide = Presentation(outdated_guide_file)
-        # formatting_data = [SlideProcessor.extract_formatting(slide) for slide in outdated_guide_slide.slides]
-        # slide_data = SlideProcessor.generate_presentation(st.session_state.generated_text, formatting_data)
-        slide_data = SlideProcessor.create_slides(st.session_state.generated_text)
-        binary_output = BytesIO()
-        slide_data.save(binary_output)
-
-        st.subheader("Download the Updated User Guide")
-
-        # st.selectbox("Export As...",
-        #              ("DOCX", "TXT", "PDF", "PPT", "MP4"))
-
-        st.download_button(
-            "Download",
-            file_name="updated_guide.pptx",
-            data=binary_output,
-            mime="application/vnd.openxmlformats-officedocument.presentationml.presentation"
-        )
+    # if st.session_state.generated_text:
+    #     st.subheader("Updated User Guide Content")
+    #
+    #     if "edit_mode" not in st.session_state:
+    #         st.session_state.edit_mode = False
+    #
+    #     outdated_guide_compared, updated_guide_compared = DocumentProcessor.compare_texts(
+    #         st.session_state.outdated_guide_text, st.session_state.generated_text)
+    #
+    #     if st.session_state.edit_mode:
+    #         DocumentProcessor.edit_output(updated_guide_compared)
+    #     else:
+    #         DocumentProcessor.display_output(outdated_guide_compared, updated_guide_compared)
+    #
+    #     def regenerate_guide():
+    #         st.session_state.generated_text = SlideProcessor.generate_presentation_content(st.session_state.outdated_guide_text, st.session_state.reference_material_text)
+    #
+    #     st.button("Re-Generate", on_click=regenerate_guide)
+    #
+    #     # TextProcessor.select_version()
+    #
+    #     # outdated_guide_slide = Presentation(outdated_guide_file)
+    #     # formatting_data = [SlideProcessor.extract_formatting(slide) for slide in outdated_guide_slide.slides]
+    #     # slide_data = SlideProcessor.generate_presentation(st.session_state.generated_text, formatting_data)
+    #     slide_data = SlideProcessor.create_slides(st.session_state.generated_text)
+    #     binary_output = BytesIO()
+    #     slide_data.save(binary_output)
+    #
+    #     st.subheader("Download the Updated User Guide")
+    #
+    #     # st.selectbox("Export As...",
+    #     #              ("DOCX", "TXT", "PDF", "PPT", "MP4"))
+    #
+    #     st.download_button(
+    #         "Download",
+    #         file_name="updated_guide.pptx",
+    #         data=binary_output,
+    #         mime="application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    #     )
 
 
 if __name__ == "__main__":
--- utilities/slide_processor.py
+++ utilities/slide_processor.py
@@ -1,159 +1,166 @@
 import os
-import json
-from groq import Groq
-from haystack import Pipeline
-from haystack import Document
-from haystack.components.builders import PromptBuilder
-from haystack.utils import Secret
-from haystack.components.generators import OpenAIGenerator
-from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
-from haystack.document_stores.in_memory import InMemoryDocumentStore
-from pptx.util import Pt
-from utilities.prompts import *
-from pptx import Presentation
-from openai import AzureOpenAI
+from pathlib import Path
 from dotenv import load_dotenv
-from pptx.dml.color import RGBColor, _NoneColor
+from llama_index.core import Settings
+from llama_index.llms.groq import Groq
+from llama_index.core import VectorStoreIndex
+from llama_index.core import SimpleDirectoryReader
+from llama_index.readers.web import SimpleWebPageReader
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.query_engine import SubQuestionQueryEngine
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 load_dotenv()
 
 class SlideProcessor:
+    Settings.llm = Groq(model="llama-3.1-70b-versatile", api_key=os.getenv("GROQ_OPENAI_API_KEY"), temperature=0.4)
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
-    # client = AzureOpenAI(
-    #     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    #     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    #     api_version="2024-06-01"
-    # )
+    # system_prompt = """
+    # You are a helpful technical writer tasked with updating an outdated user guide for a product/service.
+    # The user will input the following documents
+    # Document 1: Outdated User Guide
+    # Document 2: Reference Material
+    # Based on the input documents, your task is to update an outdated user guide slides by incorporating relevant information from the provided reference material. Generate only the JSON data required by the user and nothing else.
+    # Do not include any additional text or explanations. Each slide should only have a {{header}}, {{content}} and wrapped in list name generated_slides. Return only the JSON data as specified below. Do not include any additional text or explanations.
+    # """
+    # prompt = query_json.replace("[[content]]", system_prompt)
+    #
+    # # TODO: Need to fix the code so _NoneColor error for font colour goes away
+    # def extract_formatting(slide):
+    #     formatting_info = []
+    #     for shape in slide.shapes:
+    #         if hasattr(shape, "text_frame"):
+    #             for paragraph in shape.text_frame.paragraphs:
+    #                 for run in paragraph.runs:
+    #                     font_color_rgb = RGBColor(0, 0, 0)
+    #
+    #                     if run.font.color.rgb is not isinstance(run.font.color.rgb, _NoneColor):
+    #                         font_color_rgb = run.font.color.rgb
+    #
+    #                     formatting_info.append({
+    #                         "text": run.text,
+    #                         "font_size": run.font.size.pt if run.font.size else None,
+    #                         "font_bold": run.font.bold,
+    #                         "font_italic": run.font.italic,
+    #                         "font_name": run.font.name,
+    #                         "font_color": font_color_rgb,
+    #                     })
+    #     return formatting_info
+    #
+    # def generate_presentation_content(outdated_guide, reference_material):
+    #     outdated_document = Document(content=outdated_guide)
+    #     reference_document = Document(content=reference_material)
+    #     docstore = InMemoryDocumentStore()
+    #     docstore.write_documents([Document(content=outdated_document.content), Document(content=reference_document.content)])
+    #
+    #     template = slides_template
+    #     query = slides_query
+    #
+    #     llm = OpenAIGenerator(api_key=Secret.from_env_var("GROQ_OPENAI_API_KEY"), api_base_url="https://api.groq.com/openai/v1", model=os.getenv("GROQ_MODEL_NAME"), generation_kwargs={"temperature": 0.2})
+    #
+    #     pipe = Pipeline()
+    #     pipe.add_component("retriever", InMemoryBM25Retriever(document_store=docstore))
+    #     pipe.add_component("prompt_builder", PromptBuilder(template=template))
+    #     pipe.add_component("llm", llm)
+    #     pipe.connect("retriever", "prompt_builder.documents")
+    #     pipe.connect("prompt_builder", "llm")
+    #
+    #     res = pipe.run({"retriever": {"query": query}, "prompt_builder": {"query": query}})
+    #
+    #
+    #     # completion = SlideProcessor.client.chat.completions.create(
+    #     #     model=os.getenv("GROQ_MODEL_NAME"),
+    #     #     messages=[
+    #     #         {"role": "system", "content": SlideProcessor.system_prompt},
+    #     #         {
+    #     #             "role": "user",
+    #     #             "content": f"Outdated User Guide:\n{outdated_guide}\n\nReference Material:\n{reference_material}",
+    #     #         },
+    #     #     ]
+    #     # )
+    #
+    #     # response_content = completion.choices[0].message.content
+    #
+    #     # Returns the LLM generated response
+    #     return res['llm']['replies'][0]
+    #
+    # # TODO: Change so the original formatting is kept
+    # @staticmethod
+    # def create_slides(response_content):
+    #     prs = Presentation()
+    #
+    #     json_rsp = json.loads(response_content)
+    #     slide_data = json_rsp.get("generated_slides")
+    #     print(slide_data)
+    #
+    #     for slide in slide_data:
+    #         slide_layout = prs.slide_layouts[1]
+    #         new_slide = prs.slides.add_slide(slide_layout)
+    #
+    #         if slide['header']:
+    #             title = new_slide.shapes.title
+    #             title.text = slide['header']
+    #
+    #         if slide['content']:
+    #             shapes = new_slide.shapes
+    #             body_shape = shapes.placeholders[1]
+    #             tf = body_shape.text_frame
+    #
+    #             content = slide['content']
+    #             if isinstance(content, list):
+    #                 content = " ".join(content)
+    #
+    #             p = tf.add_paragraph()
+    #             run = p.add_run()
+    #             run.text = content
+    #             run.font.size = Pt(18)
+    #             run.font.bold = True
+    #             run.font.name = "Calibri"
+    #
+    #     return prs
 
-    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    def save_uploaded_file(file):
+        save_folder = Path('uploaded_files/')
+        save_folder.mkdir(parents=True, exist_ok=True)
+        save_path  = save_folder / file.name
+        with open(save_path, mode='wb') as w:
+            w.write(file.getvalue())
 
-    query_json = """{
-        "input_text": "[[content]]",
-        "output_format": "json",
-        "json_structure": {
-        "generated_slides": [{"header": {header_content}, "content": {content}}]
-    }"""
+        return save_path
 
-    system_prompt = """
-    You are a helpful technical writer tasked with updating an outdated user guide for a product/service. 
-    The user will input the following documents
-    Document 1: Outdated User Guide
-    Document 2: Reference Material
-    Based on the input documents, your task is to update an outdated user guide slides by incorporating relevant information from the provided reference material. Generate only the JSON data required by the user and nothing else.
-    Do not include any additional text or explanations. Each slide should only have a {{header}}, {{content}} and wrapped in list name generated_slides. Return only the JSON data as specified below. Do not include any additional text or explanations. 
-    """
-    prompt = query_json.replace("[[content]]", system_prompt)
+    def load_document(file_path):
+        document = SimpleDirectoryReader(input_files=[file_path]).load_data()
+        return document
 
-    # TODO: Need to fix the code so _NoneColor error for font colour goes away
-    def extract_formatting(slide):
-        formatting_info = []
-        for shape in slide.shapes:
-            if hasattr(shape, "text_frame"):
-                for paragraph in shape.text_frame.paragraphs:
-                    for run in paragraph.runs:
-                        font_color_rgb = RGBColor(0, 0, 0)
+    def load_webpage(url):
+        document = SimpleWebPageReader(html_to_text=True).load_data([url])
+        return document
 
-                        if run.font.color.rgb is not isinstance(run.font.color.rgb, _NoneColor):
-                            font_color_rgb = run.font.color.rgb
+    def build_query_engine(outdated_doc, reference_doc):
+        outdated_index = VectorStoreIndex.from_documents(outdated_doc)
+        reference_index = VectorStoreIndex.from_documents(reference_doc)
 
-                        formatting_info.append({
-                            "text": run.text,
-                            "font_size": run.font.size.pt if run.font.size else None,
-                            "font_bold": run.font.bold,
-                            "font_italic": run.font.italic,
-                            "font_name": run.font.name,
-                            "font_color": font_color_rgb,
-                        })
-        return formatting_info
+        outdated_engine = outdated_index.as_query_engine(similarity_top_k=2)
+        reference_engine = reference_index.as_query_engine(similarity_top_k=2)
 
-    def generate_presentation_content(outdated_guide, reference_material):
-        outdated_document = Document(content=outdated_guide)
-        reference_document = Document(content=reference_material)
-        docstore = InMemoryDocumentStore()
-        docstore.write_documents([Document(content=outdated_document.content), Document(content=reference_document.content)])
+        query_engine_tools = [
+            QueryEngineTool(
+                query_engine=outdated_engine,
+                metadata=ToolMetadata(
+                    name="Outdated Guide",
+                    description="Contains the material from an Outdated User Guide",
+                ),
+            ),
+            QueryEngineTool(
+                query_engine=reference_engine,
+                metadata=ToolMetadata(
+                    name="Reference Guide",
+                    description="Contains the material for updating the Outdated User Guide",
+                ),
+            ),
+        ]
 
-        template = slides_template
-        query = slides_query
+        s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=query_engine_tools)
 
-        llm = OpenAIGenerator(api_key=Secret.from_env_var("GROQ_OPENAI_API_KEY"), api_base_url="https://api.groq.com/openai/v1", model=os.getenv("GROQ_MODEL_NAME"), generation_kwargs={"temperature": 0.2})
-
-        pipe = Pipeline()
-        pipe.add_component("retriever", InMemoryBM25Retriever(document_store=docstore))
-        pipe.add_component("prompt_builder", PromptBuilder(template=template))
-        pipe.add_component("llm", llm)
-        pipe.connect("retriever", "prompt_builder.documents")
-        pipe.connect("prompt_builder", "llm")
-
-        res = pipe.run({"retriever": {"query": query}, "prompt_builder": {"query": query}})
-
-
-        # completion = SlideProcessor.client.chat.completions.create(
-        #     model=os.getenv("GROQ_MODEL_NAME"),
-        #     messages=[
-        #         {"role": "system", "content": SlideProcessor.system_prompt},
-        #         {
-        #             "role": "user",
-        #             "content": f"Outdated User Guide:\n{outdated_guide}\n\nReference Material:\n{reference_material}",
-        #         },
-        #     ]
-        # )
-
-        # response_content = completion.choices[0].message.content
-
-        # Returns the LLM generated response
-        return res['llm']['replies'][0]
-
-    # TODO: Change so the original formatting is kept
-    @staticmethod
-    def create_slides(response_content):
-        prs = Presentation()
-
-        json_rsp = json.loads(response_content)
-        slide_data = json_rsp.get("generated_slides")
-        print(slide_data)
-
-        for slide in slide_data:
-            slide_layout = prs.slide_layouts[1]
-            new_slide = prs.slides.add_slide(slide_layout)
-
-            if slide['header']:
-                title = new_slide.shapes.title
-                title.text = slide['header']
-
-            if slide['content']:
-                shapes = new_slide.shapes
-                body_shape = shapes.placeholders[1]
-                tf = body_shape.text_frame
-
-                content = slide['content']
-                if isinstance(content, list):
-                    content = " ".join(content)
-
-                p = tf.add_paragraph()
-                run = p.add_run()
-                run.text = content
-                run.font.size = Pt(18)
-                run.font.bold = True
-                run.font.name = "Calibri"
-
-        return prs
-
-    # May not need this
-    def extract_pptx_to_json(file):
-        presentation = Presentation(file)
-        slides_data = []
-
-        for slide_number, slide in enumerate(presentation.slides):
-            slide_content = {
-                "slide_number": slide_number + 1,
-                "shapes": []
-            }
-
-            for shape in slide.shapes:
-                if hasattr(shape, "text") and shape.text.strip():
-                    slide_content["shapes"].append({"text": shape.text})
-
-            slides_data.append(slide_content)
-
-        json_data = json.dumps((slides_data), indent=4)
-        return json_data
+        return s_engine
