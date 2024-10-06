ec0a6054af9c15eae6cbd76696dc6e3e7234b530
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2024-10-06 23:39:23 +1300

    feat[slides]: Add create feature

--- pages/slide.py
+++ pages/slide.py
@@ -1,70 +1,23 @@
-from io import BytesIO
 import streamlit as st
-from utilities.slide_rag import SlideRag
+
+from utilities.powerpoint import PowerPoint
 
 
 def main():
-    st.set_page_config(page_title="Update PowerPoint", layout="wide")
+    st.set_page_config(page_title="PowerPoint", layout="wide")
 
-    # Set app header
-    st.header("Update PowerPoint")
+    tab1, tab2 = st.tabs(["Update", "Create"])
 
-    # Upload both outdated user guide and reference material
-    outdated_guide_file = st.file_uploader("Choose the outdated user guide document",
-                                           type=["txt", "pdf", "docx", "pptx"])
-    reference_material_file = st.file_uploader("Choose the reference material document",
-                                               type=["txt", "pdf", "docx", "pptx"])
-    webpage_link = st.text_input("Input webpage link")
+    with tab1:
+        # Set app header
+        st.header("Update PowerPoint")
 
-    if st.button("Generate Updated Guide"):
-        if outdated_guide_file is not None:
-            st.session_state.outdated_path = SlideRag.save_uploaded_file(outdated_guide_file)
+        PowerPoint.update_slide()
 
-            if reference_material_file or webpage_link is not None:
-                if reference_material_file:
-                    reference_path = SlideRag.save_uploaded_file(reference_material_file)
-                    st.session_state.reference_document = SlideRag.load_reference(reference_path)
+    with tab2:
+        st.header("Create PowerPoint")
 
-                if webpage_link:
-                    st.session_state.reference_document = SlideRag.load_webpage(webpage_link)
-            else:
-                st.error("Please upload either reference material or webpage link.")
-
-        if 'outdated_path' and 'reference_document' in st.session_state:
-            st.session_state.ppt, st.session_state.contents_map = SlideRag.generate_updated(
-                st.session_state.outdated_path, st.session_state.reference_document)
-            SlideRag.compare_difference()
-
-        else:
-            st.error("Please upload outdated material.")
-
-    if 'before' and 'after' in st.session_state:
-        st.subheader("Updated User Guide Content")
-
-        if "edit_mode" not in st.session_state:
-            st.session_state.edit_mode = False
-
-        if not st.session_state.edit_mode:
-            SlideRag.display_difference(st.session_state.before, st.session_state.after)
-            if st.button("Edit"):
-                st.session_state.edit_mode = True
-                st.rerun()
-        else:
-            SlideRag.edit_output()
-
-        SlideRag.modify_powerpoint()
-        binary_output = BytesIO()
-        st.session_state.ppt.save(binary_output)
-
-        st.subheader("Download the Updated User Guide")
-
-        st.download_button(
-            "Download",
-            file_name="updated_guide.pptx",
-            data=binary_output,
-            mime="application/vnd.openxmlformats-officedocument.presentationml.presentation"
-        )
-
+        PowerPoint.create_slide()
 
 if __name__ == "__main__":
     main()
--- utilities/slide_generator.py
+++ utilities/slide_generator.py
@@ -1,10 +1,7 @@
+import json
 from dotenv import load_dotenv
-import streamlit as st
 import os
-import re
-from pptx.util import Inches, Pt
 from pathlib import Path
-from html import unescape
 from llama_index.core import Settings
 from llama_index.llms.groq import Groq
 from llama_parse import LlamaParse
@@ -37,7 +34,27 @@ Instructions:
 6. Do not include any additional content.
 7. Do not include any explanations, introductions, or additional text.
 """
-class SlideRag:
+
+create_sys_prompt = """You are tasked with creating a new PowerPoint presentation for the user based on the provided reference materials and the prompt input by the user.
+
+**Instructions:**
+1. Carefully review all reference materials provided.
+2. Analyze the user's prompt to understand their requirements and expectations.
+3. Create a PowerPoint presentation structured in the following JSON format:
+
+```json
+{
+  "slides": [
+    {
+      "heading": "{heading}",
+      "content": "{content}"
+    }
+  ]
+}
+4. Do not include any additional content.
+5. Do not include any explanations, introductions, or additional text.
+"""
+class SlideGenerator:
     Settings.llm = Groq(model="llama-3.1-70b-versatile", api_key=os.getenv("GROQ_API_KEY"), temperature=0.4)
 
     config_list = [
@@ -103,7 +120,7 @@ class SlideRag:
                     new material should be added, display 'yes'"""
 
                     review_user_message = {"content": review_context, "role": "user"}
-                    reply = SlideRag.agent.generate_reply(messages=[review_system_message, review_user_message])
+                    reply = SlideGenerator.agent.generate_reply(messages=[review_system_message, review_user_message])
 
                     for paragraph in shape.text_frame.paragraphs:
                         if not paragraph.text.strip():
@@ -126,7 +143,7 @@ class SlideRag:
                                 to be removed, display 'remove'."""
 
                                 generate_user_message = {"content": generate_context, "role": "user"}
-                                reply = SlideRag.agent.generate_reply(messages=[generate_system_message, generate_user_message])
+                                reply = SlideGenerator.agent.generate_reply(messages=[generate_system_message, generate_user_message])
 
                                 if reply:
                                     if reply['content'] != "n/a" and reply['content'] != "remove":
@@ -137,134 +154,20 @@ class SlideRag:
 
         return ppt, contents_map
 
-    def compare_difference(**kwargs):
-        before = []
-        after = []
+    def generate_new(reference_docs, prompt):
+        generate_sys_message = {"content": create_sys_prompt, "role": "system"}
 
-        before_list = list(st.session_state.contents_map.keys())
-        after_list = list(st.session_state.contents_map.values())
+        context = f"""Based on the given user prompt and the reference materials, generate a new powerpoint. 
+        This is the user prompt: {prompt}
+        Here is the reference material: {reference_docs}"""
+        generate_user_message = {"content": context, "role": "user"}
 
-        max_length = max(len(before_list), len(after_list))
+        reply = SlideGenerator.agent.generate_reply(messages=[generate_sys_message, generate_user_message])
 
-        for i in range(max_length):
-            before_text = before_list[i] if i < len(before_list) else ""
-            after_text = after_list[i] if i < len(after_list) else ""
+        return reply
 
-            if before_text == after_text:
-                before.append(before_text)
-                after.append(after_text)
+    def extract_json(reply):
+        json_rsp = json.loads(reply['content'])
+        slide_data = json_rsp.get("slides")
 
-            elif after_text == "":
-                before.append(f'<span style="color: #FFCCCB;">{before_text}</span>')
-                after.append(after_text)
-
-            elif before_text == "":
-                before.append(before_text)
-                after.append(f'<span style="color: #90EE90;">{after_text}</span>')
-
-            else:
-                before.append(f'<span style="color: #FFCCCB;">{before_text}</span>')
-                after.append(f'<span style="color: #90EE90;">{after_text}</span>')
-
-        st.session_state.before = before
-        st.session_state.after = after
-
-    def display_difference(before, after):
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.subheader("Outdated User Guide")
-            st.markdown("<br>".join(before), unsafe_allow_html=True)
-
-        with col2:
-            st.subheader("Updated User Guide")
-            st.markdown("<br>".join(after), unsafe_allow_html=True)
-
-    def strip_html_tags(**kwargs):
-        clean_text = ""
-        for text in st.session_state.after:
-            cleaned = re.sub(r"<[^>]+>", "", text)
-            cleaned = unescape(cleaned)
-            clean_text += cleaned + "\n"
-
-        return clean_text
-
-
-    def edit_output(**kwargs):
-        clean_text = SlideRag.strip_html_tags()
-
-        edited_text = st.text_area(
-            "Edit the content",
-            height=800,
-            value=clean_text,
-            key="edit_area",
-        )
-
-        if st.button("Save"):
-            SlideRag.save_edit(edited_text)
-            st.rerun()
-
-    def save_edit(edited_text):
-        st.session_state.edit_mode = False
-
-        st.session_state.after = edited_text.split("\n")
-
-        i = 1
-        new_map = {}
-        for before, after in zip(st.session_state.before, st.session_state.after):
-            if not after.strip():
-                new_map[f'New Line {i}'] = "\n"
-                i += 1
-            else:
-                new_map[before] = after
-
-        i = 1
-        if len(st.session_state.after) > len(st.session_state.before):
-            for extra_text in st.session_state.after[len(st.session_state.before):]:
-                new_map[f'Extra {i}'] = extra_text
-                i += 1
-
-        if len(st.session_state.before) > len(st.session_state.after):
-            for extra_before_text in st.session_state.before[len(st.session_state.after):]:
-                new_map[extra_before_text] = ""
-
-        st.session_state.contents_map = new_map
-
-        SlideRag.compare_difference()
-
-    def modify_powerpoint(**kwargs):
-        extra_text = ""
-        for key, value in st.session_state.contents_map.items():
-            for slide in st.session_state.ppt.slides:
-                for shape in slide.shapes:
-                    if not shape.has_text_frame:
-                        continue
-                    for paragraph in shape.text_frame.paragraphs:
-                        if key in paragraph.text:
-                            if "New Line" in key:
-                                new_paragraph = shape.text_frame.add_paragraph()
-                                new_paragraph.text = ""
-                            if paragraph.text != value:
-                                paragraph.text = value
-            if "Extra" in key:
-                extra_text += value + "\n"
-
-        if extra_text:
-            new_slide_layout = st.session_state.ppt.slide_layouts[6]
-            new_slide = st.session_state.ppt.slides.add_slide(new_slide_layout)
-
-            left = top = Inches(1)
-            height = st.session_state.ppt.slide_height - 2 * top
-            width = st.session_state.ppt.slide_width - 2 * left
-
-            tx_box = new_slide.shapes.add_textbox(left, top, width, height)
-            tf = tx_box.text_frame
-
-            p = tf.add_paragraph()
-            p.text = "New Content"
-            p.font.size = Pt(40)
-            p.font.bold = True
-
-            p = tf.add_paragraph()
-            p.text = extra_text
-            p.font.size = Pt(12)
+        return slide_data
