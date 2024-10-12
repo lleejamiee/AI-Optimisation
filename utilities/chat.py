bce842f20f9bebeedb0ae779fd2b036909e9d4b6
Philip Jin <philipjin4@gmail.com>
2024-10-13 03:46:28 +1300

    Add RAG to chatbot

--- utilities/chat.py
+++ utilities/chat.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from groq import Groq
 from utilities.chat_rag import ChatRAG
-from utilities.prompts import chat_bot
+from utilities.prompts import chatbot_system
 import os
 from dotenv import load_dotenv
 
-groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+load_dotenv()
 
 
 def parse_groq_stream(stream):
@@ -15,10 +15,38 @@ def parse_groq_stream(stream):
                 yield chunk.choices[0].delta.content
 
 
+def rag_response(prompt):
+    response = st.session_state['rag'].rag_query(prompt)
+
+    return response
+
+
+def clear_chat_history():
+    st.session_state.messages = []
+
+
 def run_chatbot():
+    if 'client' not in st.session_state:
+        st.session_state.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+    if 'rag' not in st.session_state:
+        st.session_state.rag = ChatRAG()
+
+    # Upload file
+    with st.container(border=True):
+        st.subheader("Upload Training Material")
+        outdated_guide_file = st.file_uploader("Choose the outdated user guide document", type=["docx", "pdf"])
+
+    if st.button("Upload"):
+        if outdated_guide_file is None:
+            st.error("Please upload a file")
+        else:
+            clear_chat_history()
+            st.session_state['rag'].create_rag_retriever(outdated_guide_file)
+
     # Set a default model
     if "groq_model" not in st.session_state:
-        st.session_state["groq_model"] = "llama3-8b-8192"
+        st.session_state["groq_model"] = "llama-3.2-90b-text-preview"
 
     # Initialize chat history
     if "messages" not in st.session_state:
@@ -26,29 +54,29 @@ def run_chatbot():
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
-        if message["role"] == "user":
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-        elif message["role"] == "assistant":
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     # Accept user input
     if prompt := st.chat_input("What is up?"):
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
+
         # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
 
+        # Retrieve context with prompt
+        rag_prompt = rag_response(prompt)
+
         # Send user message
         with st.chat_message("assistant"):
-            stream = groq_client.chat.completions.create(
+            stream = st.session_state.client.chat.completions.create(
                 model=st.session_state["groq_model"],
                 messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    *[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-                    {"role": "user", "content": prompt}
+                    {"role": "system", "content": chatbot_system},
+                    *st.session_state.messages,
+                    {"role": "user", "content": rag_prompt}  # Include RAG context here
                 ],
                 temperature=0,
                 stream=True,
